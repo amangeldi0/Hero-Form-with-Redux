e0bbc2bbbb0eed71d4758d1138b27af907c971a2
amangeldi0 <amanimanbekov12@gmail.com>
2022-10-21 19:46:52 +0600

    fetch func, map filter

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -14,16 +14,51 @@ import Spinner from "../spinner/Spinner";
 // Представьте, что вы попросили бэкенд-разработчика об этом
 
 const HeroesFilters = () => {
+    const dispatch = useDispatch()
+    const {filters, filterLoadingStatus, activeFilter} = useSelector(state => state)
+    const {request} = useHttp()
+
+    useEffect(() => {
+        dispatch(filtersFetching())
+        request('http://localhost:3001/filters')
+            .then(data => dispatch(filtersFetched(data)))
+            .catch(() => dispatch(filtersFetchingError()))
+    }, [])
+
+
+    if (filterLoadingStatus === 'loading'){
+        return <Spinner />
+    }else if(filterLoadingStatus === 'error'){
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    }
+
+    const filterMap = (arr) => {
+        if (arr.length === 0){
+            return <h5 className="text-center mt-5">Фильтры не найдены</h5>
+        }
+
+        return arr.map(({name, className, label}) => {
+
+            const btnClass = classNames('btn', className, {
+                'active': name === activeFilter
+            });
+
+            return <button
+                key={name}
+                id={name}
+                className={btnClass}
+                onClick={() => dispatch(activeFilterChanged(name))}
+            >{label}</button>
+        })
+    }
+    const element = filterMap(filters)
+
     return (
         <div className="card shadow-lg mt-4">
             <div className="card-body">
                 <p className="card-text">Отфильтруйте героев по элементам</p>
                 <div className="btn-group">
-                    <button className="btn btn-outline-dark active">Все</button>
-                    <button className="btn btn-danger">Огонь</button>
-                    <button className="btn btn-primary">Вода</button>
-                    <button className="btn btn-success">Ветер</button>
-                    <button className="btn btn-secondary">Земля</button>
+                    {element}
                 </div>
             </div>
         </div>
