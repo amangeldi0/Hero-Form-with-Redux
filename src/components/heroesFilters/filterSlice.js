206671b31f213a0949a4dc6c73e639849fe562b9
amangeldi0 <amanimanbekov12@gmail.com>
2022-10-23 16:25:04 +0600

    use createSlice create new reducer with action in one file

---
+++ src/components/heroesFilters/filterSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+    filters: [],
+    filtersLoadingStatus: 'idle',
+    activeFilter: 'all'
+}
+
+const filters = createSlice({
+    name: 'filters',
+    initialState,
+    reducers: {
+        filtersFetching: state => {state.filtersLoadingStatus = 'loading'},
+        filtersFetched: (state, action) => {
+            state.filters = action.payload
+            state.filtersLoadingStatus = 'idle'
+        },
+        filtersFetchingError: state => {state.filtersLoadingStatus = 'error'},
+        activeFilterChanged: (state, action) => {state.activeFilter = action.payload}
+    }
+
+})
+const {reducer, actions} = filters
+
+export const {
+    filtersFetching,
+    filtersFetched,
+    filtersFetchingError,
+    activeFilterChanged
+} = actions
+
+export default reducer;
