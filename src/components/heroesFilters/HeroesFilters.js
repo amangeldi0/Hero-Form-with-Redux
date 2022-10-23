f0db6688b022c9d3b1e7fddf41659a17711f5ffb
amangeldi0 <amanimanbekov12@gmail.com>
2022-10-23 12:56:55 +0600

    clean code and change request func with redux-thunk

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -2,7 +2,7 @@ import {useHttp} from '../../hooks/http.hook';
 import { useEffect } from "react";
 import { useSelector, useDispatch} from "react-redux";
 import classNames from 'classnames';
-import { filtersFetched, filtersFetchingError, filtersFetching, activeFilterChanged} from "../../actions";
+import { fetchFilters, activeFilterChanged} from "../../actions";
 import Spinner from "../spinner/Spinner";
 
 const HeroesFilters = () => {
@@ -11,12 +11,8 @@ const HeroesFilters = () => {
     const {request} = useHttp()
 
     useEffect(() => {
-        dispatch(filtersFetching())
-        request('http://localhost:3001/filters')
-            .then(data => dispatch(filtersFetched(data)))
-            .catch(() => dispatch(filtersFetchingError()))
-    }, [])
-
+        dispatch(fetchFilters(request))
+    }, [request])
 
     if (filterLoadingStatus === 'loading'){
         return <Spinner />
